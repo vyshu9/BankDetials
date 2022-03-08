817862dab43744487183f764fa2a95b33fbf4c23
vyshu9 <100415357+vyshu9@users.noreply.github.com>
2022-03-08 15:33:59 +0530

    Add files via upload

---
+++ BankingDiscoveryService/src/main/java/com/spring/bank/discovery/DiscoveryServerApplication.java
@@ -0,0 +1,9 @@
+package com.spring.bank.discovery;
+
+import org.springframework.boot.SpringApplication;
+
+public class DiscoveryServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+}
